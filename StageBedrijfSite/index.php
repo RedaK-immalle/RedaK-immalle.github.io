d4ec7370403f487809f3d61697bbf72370757c47
Whick <unitme.y@gmail.com>
2025-01-10 09:19:56 +0100

    Initial Stagebedrijf Commit

---
+++ StageBedrijfSite/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Sarala:wght@400;700&display=swap" rel="stylesheet">
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Stage bedrijf</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <nav>
+			<!-- Main Logo -->
+			<div id="logo">Stage Bedrijf</div>
+			<!-- Main Buttons -->
+			<ul id="main-nav-buttons">
+				<li><a href="./index.php">Home</a></li>
+				<li><a href="./index.php">Contact</a></li>
+				<li><a href="./index.php">About Us</a></li>
+			</ul>
+
+			<!-- Socials -->
+			<ul id="social-nav-buttons">
+				<li><img class="social-nav-img" src="Icons/envelope.png"/></li>
+				<li><img class="social-nav-img" src="Icons/phone-call.png"/></li>
+				<li><img class="social-nav-img" src="Icons/linkedin.png"/></li>
+			</ul>
+		</nav>
+    </body>
+</html>
